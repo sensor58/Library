6ba91934b2b8088162c0e23ef439ce9e9797d064
Matus <matus.slovik@gmail.com>
2016-03-11 00:06:32 +0100

    Login screen

--- app/src/main/java/fiit/mtaa/library/LoginScreen.java
+++ app/src/main/java/fiit/mtaa/library/LoginScreen.java
@@ -1,5 +1,9 @@
 package fiit.mtaa.library;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,52 +12,54 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_login;
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login_screen, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        if((username.getText().toString().equals("Margita")) && (password.getText().toString().equals("Ziguli")))  {
+            startActivity(new Intent(this, OverviewScreen.class));
         }
+        else {
+            String message = null;
 
-        return super.onOptionsItemSelected(item);
+            if((!username.getText().toString().isEmpty()) && (!password.getText().toString().isEmpty())) {
+                showDialog("Incorrect username or password! Please try again.");
+            }
+            else {
+                showDialog("Both username and password must be filled!");
+            }
+        }
     }
 
-    private void Test() {
-        String bla = "test";
-        String blabla = "test2";
+    private void showDialog(String message) {
+        new AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }).show();
     }
-
-
 }
